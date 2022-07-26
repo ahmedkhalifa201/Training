1bb9bd3d331d3f03d94f4c9a18b1090dad407dc7
ahmedkhalifa201 <110069889+ahmedkhalifa201@users.noreply.github.com>
2022-07-27 01:40:41 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from 'react'
-
-
-import {Text, View, StyleSheet } from 'react-native'
-import { RFPercentage } from 'react-native-responsive-fontsize';
-import {  Header } from './components';
-import { FONTS, SIZES } from './constants';
-
+import Templete from './Test/Templete';
 
 
 
@@ -13,33 +7,12 @@ const App = () => {
 
   return (
     <>
-      <View style={styles.main_View}>
+    
+     <Templete/>
 
-        <Header
-          Header_name='الصفحه الرئيسيه'
-          Icon_name='back'
-        />
-
-        <View style={{height:50,width:50,borderWidth:1,borderRadius:RFPercentage(2)}}></View>
-        {/* 
-
-           code Here.........................................
-
-        */}
-
-      </View>
     </>
   )
 }
 
-const styles = StyleSheet.create({
-
-  main_View: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center'
-  },
-
-})
 
 export default App
